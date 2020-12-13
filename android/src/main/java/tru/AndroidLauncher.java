ad28fc1ab6ce7139a219fb45a4fa07cc757d2100
Tommie Nygren <tommie.nygren@gmail.com>
2020-12-13 16:48:08 +0100

    First commit of Turbo-Rocket-Ultra

---
+++ android/src/main/java/tru/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package tru;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import tru.MainGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), configuration);
+	}
+}
