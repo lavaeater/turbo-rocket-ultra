b495a7cff34d94ad89f0fd42f556e6a04326cf1b
Tommie Nygren <tommie.nygren@gmail.com>
2021-03-14 09:11:45 +0100

    Character Sprites Removed State Component and moved its properties into the sprite component for characters

--- core/src/main/kotlin/ecs/components/CharacterAnimStateComponent.kt
+++
@@ -1,8 +0,0 @@
-package ecs.components
-
-import tru.AnimState
-import tru.SpriteDirection
-import java.awt.Component
-
-class CharacterAnimStateComponent(var currentAnimState : AnimState = AnimState.Idle, var currentDirection: SpriteDirection.South) : Component {
-}
--- core/src/main/kotlin/ecs/components/CharacterSpriteComponent.kt
+++ core/src/main/kotlin/ecs/components/CharacterSpriteComponent.kt
@@ -3,6 +3,9 @@ package ecs.components
 import com.badlogic.ashley.core.Component
 import tru.AnimState
 import tru.LpcCharacterAnim
+import tru.SpriteDirection
 
-class CharacterSpriteComponent(private val anims: Map<AnimState, LpcCharacterAnim>) : Component {
+class CharacterSpriteComponent(val anims: Map<AnimState, LpcCharacterAnim>) : Component {
+    var currentAnimState : AnimState = anims.values.first().state
+    var currentDirection: SpriteDirection = SpriteDirection.South
 }
