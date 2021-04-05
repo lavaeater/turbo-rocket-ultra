ef7b744e564eb652ff8b78ed1a0c25654c3e724e
Tommie Nygren <tommie.nygren@gmail.com>
2021-04-05 20:36:53 +0200

    HUD v2 Showing lives for player in the form of mini-versions of character sprite

--- core/src/main/kotlin/ui/UserInterface.kt
+++ core/src/main/kotlin/ui/UserInterface.kt
@@ -66,34 +66,22 @@ class UserInterface(
             for ((i, p) in players.values.withIndex()) {
                 children.add(
                     TextActor(
-                        "Player $i",
+                        "Player ${i + 1}",
                         vec2(i * 10f, 10f)
                     )
                 )
                 children.add(
-                    SpacedContainer(vec2(40f, 0f), vec2(i * 10f, 50f)).apply {
-                        children.add(
-                            TextureActor(
-                                p.entity.getComponent<CharacterSpriteComponent>().currentAnim.keyFrames.first(),
-                                vec2(0f, 0f),
-                                0.5f
-                            )
-                        )
-                        children.add(
-                            TextureActor(
-                                p.entity.getComponent<CharacterSpriteComponent>().currentAnim.keyFrames.first(),
-                                vec2(0f, 0f),
-                                0.5f
-                            )
-                        )
-                        children.add(
-                            TextureActor(
-                                p.entity.getComponent<CharacterSpriteComponent>().currentAnim.keyFrames.first(),
-                                vec2(0f, 0f),
-                                0.5f
-                            )
-                        )
-                    }
+                    DataBoundRepeatingTextureActor(
+                        { p.lives },
+                        20f,
+                        0f,
+                        p
+                            .entity
+                            .getComponent<CharacterSpriteComponent>()
+                            .currentAnim
+                            .keyFrames
+                            .first(), vec2(0f, 10f), 0.5f
+                    )
                 )
             }
         }
@@ -120,9 +108,13 @@ abstract class LeafActor(val position: Vector2) : SimpleActor {
 class SimpleContainer(position: Vector2) : ContainerBaseActor(position)
 
 class SpacedContainer(private val offset: Vector2, position: Vector2) : ContainerBaseActor(position) {
+    val spaceVector = vec2()
     override fun render(batch: Batch, parentPosition: Vector2) {
         for ((index, child) in children.withIndex()) {
-            child.render(batch, vec2(parentPosition.x + index * offset.x, parentPosition.y -  index * offset.y))
+            child.render(
+                batch,
+                spaceVector.set(parentPosition.x + index * offset.x, parentPosition.y - index * offset.y)
+            )
         }
     }
 }
@@ -142,7 +134,44 @@ class TextActor(
 
 }
 
-class TextureActor(
+open class DataBoundRepeatingTextureActor(
+    val repeaterValue: () -> Int,
+    offsetX: Float,
+    offsetY: Float,
+    textureRegion: TextureRegion,
+    position: Vector2,
+    scale: Float) : RepeatingTextureActor(
+    repeaterValue(),
+    offsetX,
+    offsetY,
+    textureRegion,
+    position,
+    scale) {
+
+    override val repeatFor get() = repeaterValue()
+}
+
+open class RepeatingTextureActor(
+    open val repeatFor: Int,
+    val offsetX: Float,
+    val offsetY: Float,
+    textureRegion: TextureRegion,
+    position: Vector2,
+    scale: Float
+) : TextureActor(
+    textureRegion,
+    position,
+    scale
+) {
+
+    val spaceVector = vec2()
+    override fun render(batch: Batch, parentPosition: Vector2) {
+        for (n in 0 until repeatFor)
+            super.render(batch, spaceVector.set(parentPosition.x + n * offsetX, parentPosition.y + n * offsetY))
+    }
+}
+
+open class TextureActor(
     val textureRegion: TextureRegion,
     position: Vector2,
     val scale: Float = 1f
