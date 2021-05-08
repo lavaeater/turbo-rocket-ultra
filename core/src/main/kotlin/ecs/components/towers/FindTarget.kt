9adae05bff4a6c4a7343a677f9dc32d6ed6e9a60
Tommie Nygren <tommie.nygren@gmail.com>
2021-05-08 16:39:13 +0200

    Towers Amazingly, the towers worked perfectly from the start.

--- core/src/main/kotlin/ecs/components/towers/FindTarget.kt
+++ core/src/main/kotlin/ecs/components/towers/FindTarget.kt
@@ -2,9 +2,9 @@ package ecs.components.towers
 
 import ecs.components.ai.TaskComponent
 
-class FindTarget(var radius: Float = 10f): TaskComponent(){
+class FindTarget(var radius: Float = 20f): TaskComponent(){
     override fun reset() {
         super.reset()
-        radius = 10f
+        radius = 20f
     }
 }
