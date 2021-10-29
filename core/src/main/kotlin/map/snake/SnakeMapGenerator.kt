0fa85da7a8c1dbaac68a38425a9aad84561527a5
Tommie Nygren <tommie.nygren@gmail.com>
2021-10-29 09:21:12 +0200

    Whaat

--- core/src/main/kotlin/map/snake/SnakeMapGenerator.kt
+++ core/src/main/kotlin/map/snake/SnakeMapGenerator.kt
@@ -1,20 +1,160 @@
 package map.snake
 
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Vector2
+import ecs.components.graphics.renderables.RenderableTextureRegion
+import ecs.components.graphics.renderables.RenderableTextureRegions
+import tru.Assets
+
+fun <T> List<T>.random(): T {
+    return this[(0 until this.size).random()]
+}
+
 class SnakeMapGenerator {
     fun generate() {
-        /*
 
-         */
+        val size = (10..20).random()
+        var previousDirection: MapDirection = MapDirection.North
+        var currentDirection: MapDirection = MapDirection.North
+        var previousSection = SnakeMapSection()
+        val snakeMapManager = SnakeMapManager(previousSection)
+        for (i in 0..size) {
+            if (i != 0) {
+                currentDirection =
+                    MapDirection.directions.filter { it != MapDirection.opposing[previousDirection] }.random()
+            }
+            val currentSection = SnakeMapSection()
+            currentSection.connections[MapDirection.opposing[currentDirection]!!] = previousSection
+            previousSection.connections[currentDirection] = currentSection
+            previousSection = currentSection
+        }
     }
 }
 
 sealed class MapDirection {
-    object North: MapDirection()
-    object East: MapDirection()
-    object South: MapDirection()
-    object West: MapDirection()
+    object North : MapDirection()
+    object East : MapDirection()
+    object South : MapDirection()
+    object West : MapDirection()
+    companion object {
+        val opposing = mapOf(North to South, South to North, East to West, West to East)
+        val directions: List<MapDirection> = listOf(North, East, South, West)
+    }
+}
+
+sealed class TileAlignment {
+    object Center: TileAlignment()
+    object Top : TileAlignment()
+    object Bottom : TileAlignment()
+    object Left : TileAlignment()
+    object Right : TileAlignment()
+    object TopLeft : TileAlignment()
+    object TopRight : TileAlignment()
+    object BottomLeft : TileAlignment()
+    object BottomRight : TileAlignment()
+
+    companion object {
+        val directionAlignment = mapOf(
+            MapDirection.North to Top,
+            MapDirection.East to Right,
+            MapDirection.South to Bottom,
+            MapDirection.West to Left)
+        val alignmentDirection = directionAlignment.entries.associateBy ({ it.value }) {it.key}
+    }
+}
+
+class SnakeMapManager(val startSection: SnakeMapSection) {
+    /*
+    We use the worldcenter
+    to keep track of which section we are inside. Very useful
+
+     */
+
+
+    fun render(batch: Batch, delta: Float, worldCenter: Vector2) {
+
+    }
 }
 
 class SnakeMapSection(val connections: MutableMap<MapDirection, SnakeMapSection> = mutableMapOf()) {
+    /*
+    All sections consist of width x height tiles, and
 
+    the outer rows are all wall, except for the ones where there is a connection, obviously.
+     */
+    companion object {
+        const val width = 32
+        const val height = 32
+    }
+
+    val connectionAlignments = connections.keys.map { TileAlignment.directionAlignment[it]!! }
+
+    //Tiles can be changed later to add weird features or a class that is a maptile...
+    //We should probably have a maptile right now
+    val tiles = Array(width * height) {
+        val x = it / width
+        val y = it % height
+        val tileAlignment = when (x) {
+            0 -> when (y) {
+                0 -> TileAlignment.TopLeft
+                height - 1 -> TileAlignment.BottomLeft
+                else -> TileAlignment.Left
+            }
+            width - 1 -> when (y) {
+                0 -> TileAlignment.TopRight
+                height - 1 -> TileAlignment.BottomRight
+                else -> TileAlignment.Right
+            }
+            else -> when (y) {
+                0 -> TileAlignment.Top
+                height - 1 -> TileAlignment.Bottom
+                else -> TileAlignment.Center
+            }
+        }
+
+        return@Array when(tileAlignment) {
+            TileAlignment.Bottom -> if(connectionAlignments.contains(tileAlignment)) MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.floorTiles.random())))
+            ) else MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.wallTiles.random())))
+            )
+            TileAlignment.BottomLeft -> MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.wallTiles.random())))
+            )
+            TileAlignment.BottomRight -> MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.wallTiles.random())))
+            )
+            TileAlignment.Center -> MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.floorTiles.random())))
+            )
+            TileAlignment.Left -> if(connectionAlignments.contains(tileAlignment)) MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.floorTiles.random())))
+            ) else MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.wallTiles.random())))
+            )
+            TileAlignment.Right -> if(connectionAlignments.contains(tileAlignment)) MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.floorTiles.random())))
+            ) else MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.wallTiles.random())))
+            )
+            TileAlignment.Top -> if(connectionAlignments.contains(tileAlignment)) MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.floorTiles.random())))
+            ) else MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.wallEndTile)))
+            )
+            TileAlignment.TopLeft -> if(connectionAlignments.contains(tileAlignment)) MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.floorTiles.random())))
+            ) else MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.wallTiles.random())))
+            )
+            TileAlignment.TopRight -> if(connectionAlignments.contains(tileAlignment)) MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.floorTiles.random())))
+            ) else MapTile(RenderableTextureRegions(
+                listOf(RenderableTextureRegion(Assets.wallTiles.random())))
+            )
+        }
+    }
 }
+
+class MapTile(val renderables: RenderableTextureRegions, val passable: Boolean = true)
--- core/src/main/kotlin/tru/Assets.kt
+++ core/src/main/kotlin/tru/Assets.kt
@@ -41,6 +41,45 @@ object Assets : Disposable {
         )
     }
 
+    val tileTexture by lazy {
+        Texture(Gdx.files.internal("sprites/tiles/tiles.png"))
+    }
+    val tiles by lazy {
+        val ts = mutableMapOf<String, TextureRegion>()
+        for(xTile in 0..3)
+            for(yTile in 0..2) {
+                when(yTile) {
+                    0 -> ts["floor$xTile"] = TextureRegion(tileTexture,xTile * 16, yTile * 16, 16, 16)
+                    1 -> {
+                        when(xTile) {
+                            0 -> ts["wall$xTile"] = TextureRegion(tileTexture,xTile * 16, yTile * 16, 16, 16)
+                            1 -> ts["wall$xTile"] = TextureRegion(tileTexture,xTile * 16, yTile * 16, 16, 16)
+                            2 -> ts["wall_end"] = TextureRegion(tileTexture,xTile * 16, yTile * 16, 16, 16)
+                        }
+                    }
+                    2 -> {
+                        if(xTile in 0..1)
+                            ts["wall_shadow$xTile"] = TextureRegion(tileTexture,xTile * 16, yTile * 16, 16, 16)
+                    }
+                }
+            }
+        ts
+    }
+
+    val floorTiles by lazy {
+        tiles.filterKeys { it.contains("floor") }.values.toList()
+    }
+
+    val wallTiles by lazy {
+        tiles.filterKeys { it.contains("wall") && !it.contains("_end") }.values.toList()
+    }
+    val wallEndTile by lazy {
+        tiles.filterKeys { it == "wall_end" }.values.first()
+    }
+
+    val wallEndShadow by lazy { tiles["wall_shadow1"]!! }
+    val wallShadow by lazy { tiles["wall_shadow0"]!!}
+
     val towerShadow by lazy {
         TextureRegion(Texture(Gdx.files.internal("sprites/towers/tower-shadow.png")))
     }
