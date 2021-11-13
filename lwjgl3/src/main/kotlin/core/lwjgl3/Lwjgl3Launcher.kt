b12557e21fc61d7b5a200ca4fb1070c39b348dfa
Tommie Nygren <tommie.nygren@gmail.com>
2021-11-13 15:08:34 +0100

    Fullscreen

--- lwjgl3/src/main/kotlin/core/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/core/lwjgl3/Lwjgl3Launcher.kt
@@ -2,7 +2,6 @@
 
 package core.lwjgl3
 
-import com.badlogic.gdx.Graphics
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import gamestate.MainGame
@@ -11,8 +10,8 @@ import gamestate.MainGame
 fun main() {
     Lwjgl3Application(MainGame(), Lwjgl3ApplicationConfiguration().apply {
         setTitle("turbo-rocket-ultra")
-        //setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode())
-        setWindowedMode(1920, 1080)
+        setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode())
+        //setWindowedMode(1920, 1080)
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
     })
 }
