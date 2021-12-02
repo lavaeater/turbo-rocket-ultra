410a57eb6900041813295835dd10801ac688b684
Tommie Nygren <tommie.nygren@gmail.com>
2021-12-02 12:08:27 +0100

    Actually made level four

--- core/src/main/kotlin/ecs/components/enemy/EnemySpawnerComponent.kt
+++ core/src/main/kotlin/ecs/components/enemy/EnemySpawnerComponent.kt
@@ -4,7 +4,7 @@ import ecs.components.ai.CoolDownComponent
 
 class EnemySpawnerComponent : CoolDownComponent() {
     init {
-        coolDownRange = (0.1f..0.2f)
+        coolDownRange = (1f..3f)
         reset()
     }
 }
