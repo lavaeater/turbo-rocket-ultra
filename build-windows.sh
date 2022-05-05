cd96c27d4404f94933aa01a14107505380a07ee3
Tommie Nygren <tommie.nygren@gmail.com>
2022-05-05 23:40:09 +0200

    Added build scripts for more platforms

---
+++ build-windows.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -rf out-linux
+
+java -jar ../packr-all-4.0.0.jar \
+     --platform linux64 \
+     --jdk /home/tommie/.sdkman/candidates/java/current \
+     --useZgcIfSupportedOs \
+     --executable TurboRocketUltra \
+     --classpath /home/tommie/projects/games/turbo-rocket-ultra/lwjgl3/build/libs/turbo-rocket-ultra-0.0.1.jar \
+     --mainclass core.lwjgl3.Lwjgl3Launcher \
+     --vmargs Xmx1G \
+     --resources assets/* \
+     --output out-linux
+
+./out-linux/TurboRocketUltra
