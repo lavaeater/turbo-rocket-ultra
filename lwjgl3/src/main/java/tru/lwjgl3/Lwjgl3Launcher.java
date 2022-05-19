c94dc0103f929714f695d629bec6b8d74ea3c376
Tommie Nygren <tommie.nygren@gmail.com>
2022-05-19 16:35:07 +0200

    Changed to windowed mode.

--- lwjgl3/src/main/java/tru/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/tru/lwjgl3/Lwjgl3Launcher.java
@@ -17,8 +17,8 @@ public class Lwjgl3Launcher {
 	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
 		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
 		configuration.setTitle("turbo-rocket-ultra");
-		configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
-//		configuration.setWindowedMode(1600, 1600);
+//		configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+		configuration.setWindowedMode(1600, 1600);
 		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
 		return configuration;
 	}
