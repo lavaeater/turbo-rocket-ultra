9d57a97e60f6547d11d04d65ff00078cc6d55e01
Tommie Nygren <tommie.nygren@gmail.com>
2022-06-17 10:34:44 +0200

    Embryo

--- core/src/main/kotlin/ecs/systems/ai/UtilityAiSystem.kt
+++ core/src/main/kotlin/ecs/systems/ai/UtilityAiSystem.kt
@@ -1,15 +1,13 @@
 package ecs.systems.ai
 
-import ai.builders.attack
 import com.badlogic.ashley.core.Component
-import com.badlogic.ashley.core.ComponentMapper
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IteratingSystem
-import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.Fixture
 import com.badlogic.gdx.utils.Pool
+import ecs.components.ai.Path
+import ecs.components.ai.Waypoint
 import ecs.components.gameplay.TransformComponent
-import ecs.systems.graphics.GameConstants
 import factories.engine
 import factories.world
 import input.canISeeYouFromHere
@@ -125,24 +123,96 @@ class CanISeeThisConsideration<ToLookFor : Component>(
     }
 }
 
+sealed class AiActionState {
+    object Fresh: AiActionState()
+    object Running: AiActionState()
+    object Paused: AiActionState()
+    object Completed: AiActionState()
+}
+
 abstract class AiAction {
     val considerations = mutableListOf<Consideration>()
 
+    /**
+     * Action State is just used as a convenience for the task
+     * itself to see if it should reset itself or something
+     * like that.
+     *
+     * Like, a tasks score could depend on wether or not it is completed
+     *
+     */
+    var actionState: AiActionState = AiActionState.Fresh
+    private set
+
     /***
      * This is open so we can simply implement a static score for something
-     * like the Amble task. 
+     * like the Amble task - a task that will be performed unless some other task
+     * gets a higher average score - very likely since most of my considerations would be
+     * simply yes or no values
      */
     open fun score(entity: Entity): Double {
         return considerations.map { it.normalizedScore(entity) }.average()
     }
 
     abstract fun act(entity: Entity, deltaTime: Float)
+    fun pause() {
+        actionState = AiActionState.Paused
+    }
+}
+
+abstract class StaticScoreAction(private val score: Float): AiAction() {
+    override fun score(entity: Entity): Double {
+        return score.toDouble()
+    }
+}
+
+/**
+ * We could use a free-floating function to move our little entities
+ * towards a target. Very easily done.
+ */
+
+
+/**
+ * This action has a static score so unless something scores
+ * higher, this will always be executed. We might use Path components
+ * or keep
+ */
+class AmbleAiAction(score: Float):StaticScoreAction(score) {
+    override fun act(entity: Entity, deltaTime: Float) {
+        /*
+        Ambling comes in stages.
+        First we check if we have a Path - if not, we create one
+         */
+
+        /**
+         * We reuse some of the logic from the behavior tree
+         * So, we check if we have a path, if not, we create one
+         * and return. The method needs to run fast fast fast
+         * so no long hold-ups, we will return here later.
+         */
+        if(!entity.has<Path>()) {
+
+        } else if(!entity.has<Waypoint>()) {
+
+
+
+
+        } else {
+
+
+
+
+    }
 }
 
 class UtilityAiComponent : Component, Pool.Poolable {
     val actions = mutableListOf<AiAction>()
+    private var currentAction: AiAction? = null
     fun topAction(entity: Entity): AiAction? {
-        return actions.minByOrNull { it.score(entity) }
+        val potentialAction = actions.minByOrNull { it.score(entity) }
+        if(currentAction != potentialAction) {
+            currentAction.pause()
+        }
     }
 
     override fun reset() {
