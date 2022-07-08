945ddd1bed464e9323a6505c5f11a6d44491981c
Tommie Nygren <tommie.nygren@gmail.com>
2022-07-08 17:17:57 +0200

    Exploring interpolations to modify values of scores

--- core/src/main/kotlin/screens/ConceptScreen.kt
+++ core/src/main/kotlin/screens/ConceptScreen.kt
@@ -2,17 +2,27 @@ package screens
 
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.math.Interpolation
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import gamestate.GameEvent
 import gamestate.GameState
+import ktx.scene2d.actors
 import ktx.scene2d.dialog
+import ktx.scene2d.label
 import ktx.scene2d.scene2d
 import statemachine.StateMachine
 import tru.Assets
 import ui.CrawlDialog
+import kotlin.reflect.full.starProjectedType
 
 class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen(gameState) {
     private var drawPointerBall = false
+    private val valueSize = 10
+    private val values = Array(valueSize) {
+        (it * 1f / valueSize.toFloat()).toFloat()
+    }
+    private val interpolations = listOf(Interpolation.exp10, Interpolation.exp10In, Interpolation.exp10Out)
+
     private val normalCommandMap = command("Normal") {
         setUp(Input.Keys.SPACE, "Show the scrolling dialog") {
             CrawlDialog.showDialog(
@@ -36,6 +46,11 @@ class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen
 
     private val stage by lazy {
         val aStage = ktx.actors.stage(batch, ExtendViewport(800f, 600f, OrthographicCamera()))
+        aStage.actors {
+            label(interpolations.joinToString("\n") { ip -> "${ip::class.starProjectedType } " + values.joinToString { "${(ip.apply(it)* 100).toInt()}" }}).apply {
+                setFontScale(0.5f)
+            }
+        }
         aStage
     }
 
