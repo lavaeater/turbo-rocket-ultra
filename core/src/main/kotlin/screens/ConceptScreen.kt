cf0c1a80420b466158561ef4f06e7daaf8207982
Tommie Nygren <tommie.nygren@gmail.com>
2022-08-31 21:19:56 +0200

    Quaternions to the rescue, maybe?

--- core/src/main/kotlin/screens/ConceptScreen.kt
+++ core/src/main/kotlin/screens/ConceptScreen.kt
@@ -4,36 +4,138 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.Input.Buttons
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Quaternion
 import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.viewport.ExtendViewport
+import data.selectedItemListOf
 import gamestate.GameEvent
 import gamestate.GameState
 import ktx.graphics.use
+import ktx.log.info
 import ktx.math.plus
 import ktx.math.unaryMinus
 import ktx.math.vec2
 import ktx.math.vec3
 import screens.stuff.Bone3d
+import screens.stuff.selectRecursive
 import screens.stuff.toIso
 import screens.ui.KeyPress
 import statemachine.StateMachine
 import tru.Assets
 
-open class DirectionThingie {
-    val forward = vec3(0f, 0f, -25f) //towards screen
-    val up = vec3(0f, 25f, 0f)
-    val thePerp: Vector3 get() { return forward.cpy().crs(up).nor().scl(25f) }
-    val reversePerp: Vector3 get() { return -thePerp }
+fun IThing.toMap(): Map<String, IThing> {
+    return listOf(
+        this,
+        *children.asSequence().selectRecursive { children.asSequence() }.toList().toTypedArray()
+    ).associateBy { it.name }
+}
 
-    val position = vec3()
+open class Bone(name: String, localPosition: Vector3, val boneLength: Float) : Thing(name, localPosition) {
+    var boneEnd: Vector3 = vec3()
+        private set
+        get() {
+            field.set(orientation.forward.cpy().scl(boneLength).add(position))
+            return field
+        }
+}
 
-    
+sealed class CardinalOrientation(val name: String) {
+    object Up : CardinalOrientation("Up")
+    object Down : CardinalOrientation("Down")
+    object Forward : CardinalOrientation("Forward")
+    object Backwards : CardinalOrientation("Backwards")
+    object Left : CardinalOrientation("Left")
+    object Right : CardinalOrientation("Right")
+}
 
-    fun rotate(aroundUp: Float, aroundThePerp: Float, aroundForward:Float) {
-        val q = Quaternion(up.cpy().nor(), aroundUp).mul(Quaternion(thePerp.cpy().nor(), aroundThePerp)).mul(Quaternion(forward.cpy().nor(), aroundForward))
+open class Orientation {
+
+    val forward = vec3(0f, 0f, -1f) //towards screen
+    val up = vec3(0f, 1f, 0f)
+    val leftOrRight: Vector3
+        get() {
+            return forward.cpy().crs(up).nor()
+        }
+    val rightOrLeft: Vector3
+        get() {
+            return -leftOrRight
+        }
+
+    fun rotate(aroundUp: Float, left: Float, aroundForward: Float): Quaternion {
+        val q = Quaternion(up, aroundUp).mul(Quaternion(leftOrRight, left)).mul(Quaternion(forward, aroundForward))
         q.transform(forward)
         q.transform(up)
+        return q
+    }
+}
+
+interface IThing {
+    var forwardLimit: ClosedFloatingPointRange<Float>
+    var leftLimit: ClosedFloatingPointRange<Float>
+    var upLimit: ClosedFloatingPointRange<Float>
+    var name: String
+    val localPosition: Vector3
+    val orientation: Orientation
+    val forward: Vector3
+    val up: Vector3
+    val leftOrRight: Vector3
+    val reversePerp: Vector3
+    val position: Vector3
+    val allThings: Map<String, IThing>
+    val children: MutableSet<IThing>
+    var parent: IThing?
+    var rotateAroundUpEnabled: Boolean
+    var rotateAroundLeftEnabled: Boolean
+    var rotateAroundForwardEnabled: Boolean
+    fun addChild(child: IThing)
+    fun removeParent(child: IThing)
+    fun rotate(aroundUp: Float, left: Float, aroundForward: Float)
+}
+
+abstract class Thing(override var name: String, override val localPosition: Vector3) : IThing {
+    override var forwardLimit: ClosedFloatingPointRange<Float> = 0f..360f
+    override var leftLimit: ClosedFloatingPointRange<Float> = 0f..360f
+    override var upLimit: ClosedFloatingPointRange<Float> = 0f..360f
+    override val orientation = Orientation()
+    override val forward get() = orientation.forward
+    override val up get() = orientation.up
+    override val leftOrRight get() = orientation.leftOrRight
+    override val reversePerp get() = orientation.rightOrLeft
+
+    override val position: Vector3
+        get() {
+            return if (parent != null) parent!!.position + localPosition else localPosition
+        }
+    override val allThings get() = toMap()
+
+    override val children = mutableSetOf<IThing>()
+    override var parent: IThing? = null
+    override var rotateAroundUpEnabled: Boolean = true
+    override var rotateAroundLeftEnabled: Boolean = true
+    override var rotateAroundForwardEnabled: Boolean = true
+
+    override fun addChild(child: IThing) {
+        child.parent = this
+        children.add(child)
+    }
+
+    override fun removeParent(child: IThing) {
+        if (children.remove(child))
+            child.parent = null
+    }
+
+    override fun rotate(aroundUp: Float, left: Float, aroundForward: Float) {
+        val q = orientation.rotate(
+            if (rotateAroundUpEnabled) aroundUp else 0f,
+            if (rotateAroundLeftEnabled) left else 0f,
+            if (rotateAroundForwardEnabled) aroundForward else 0f)
+
+        for (child in children.asSequence().selectRecursive { children.asSequence() }.toList()) {
+            q.transform(child.localPosition)
+            q.transform(child.forward)
+            q.transform(child.up)
+        }
     }
 }
 
@@ -41,13 +143,31 @@ class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen
     override val viewport = ExtendViewport(200f, 180f)
 
     var zoom = 0f
-    var rotationY = 0f
-    var rotationX = 0f
-    var rotationZ = 0f
+    var aroundUp = 0f
+    var aroundLeft = 0f
+    var aroundForward = 0f
     var elapsedTime = 0f
     private val shapeDrawer by lazy { Assets.shapeDrawer }
 
-    val t = DirectionThingie()
+    val t = Bone("body", vec3(), 20f).apply {
+        rotate(0f, -90f, 0f)
+        addChild(Bone("left-arm-upper", vec3(10f, 0f, 0f), 10f).apply {
+            rotateAroundUpEnabled = false
+            addChild(Bone("left-arm-lower", vec3(0f, 0f, -10f), 10f).apply {
+                rotateAroundForwardEnabled = false
+                rotateAroundUpEnabled = false
+            })
+        })
+        addChild(Bone("right-arm-upper", vec3(-10f, 0f, 0f), 10f).apply {
+            rotateAroundUpEnabled = false
+            addChild(Bone("right-arm-lower", vec3(0f, 0f, -10f), 10f).apply {
+                rotateAroundForwardEnabled = false
+                rotateAroundUpEnabled = false
+            })
+        })
+    }
+
+    val thingList = selectedItemListOf(*t.allThings.values.toTypedArray())
 
     override fun render(delta: Float) {
         elapsedTime += delta
@@ -57,80 +177,74 @@ class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen
         camera.zoom = camera.zoom + 0.05f * zoom
         super.render(delta)
 
-//        boneList.selectedItem.rotateAround(rotationY, rotationX, rotationZ)
-//        boneList.selectedItem.rotateBy(rotationY, Vector3.Y)
-//        boneList.selectedItem.rotateBy(rotationX, Vector3.X)
-//        boneList.selectedItem.rotateBy(rotationZ, Vector3.Z)
-
-
-//        for (value in skeleton.bones.filterKeys { it.contains("body") }.values) {
-//            value.rotateBy(rotationY, Vector3.Y)
-//        }
-//        for (value in skeleton.bones.filterKeys { it.contains("arm-upper") }.values) {
-//            value.rotateBy(rotationX, Vector3.X)
-//        }
-
-        t.rotate(rotationY, rotationX, rotationZ)
+        if(!MathUtils.isZero(aroundUp) || !MathUtils.isZero(aroundLeft) || !MathUtils.isZero(aroundForward))
+            thingList.selectedItem.rotate(aroundUp, aroundLeft, aroundForward)
         batch.use {
             shapeDrawer.filledCircle(mousePosition, 1.5f, Color.RED)
 
-            drawThing()
+            drawThingRecursive(t)
 //            drawSkeletonRecursive(skeleton)
         }
     }
 
-    private fun drawThing() {
-        val position = vec2(-100f)
-        shapeDrawer.setColor(Color.BLUE)
-        shapeDrawer.line(position, position + t.forward.toIso(), .5f)
-        shapeDrawer.setColor(Color.RED)
-        shapeDrawer.line(position, position + t.up.toIso(), .5f)
-        shapeDrawer.setColor(Color.GREEN)
-        shapeDrawer.line(position, position + t.thePerp.toIso(), .5f)
-        shapeDrawer.setColor(Color.WHITE)
-        shapeDrawer.filledCircle(position, 1f, Color.WHITE)
-        shapeDrawer.filledCircle(position + t.forward.toIso(), 1f, Color.BLUE)
-        shapeDrawer.filledCircle(position + t.up.toIso(), 1f, Color.RED)
-        shapeDrawer.filledCircle(position + t.thePerp.toIso(), 1f, Color.GREEN)
+    private fun drawThingRecursive(thing: IThing) {
+        for (child in thing.children)
+            drawThingRecursive(child)
 
-        position.x += 50f
-        shapeDrawer.setColor(Color.BLUE)
-        shapeDrawer.line(position.x, position.y, position.x + t.forward.x, position.y + t.forward.y, .5f)
-        shapeDrawer.setColor(Color.RED)
-        shapeDrawer.line(position.x, position.y, position.x + t.up.x, position.y + t.up.y, .5f)
-        shapeDrawer.setColor(Color.GREEN)
-        shapeDrawer.line(position.x, position.y, position.x + t.thePerp.x, position.y + t.thePerp.y, .5f)
-        shapeDrawer.setColor(Color.WHITE)
-        shapeDrawer.filledCircle(position, 1f, Color.WHITE)
-        shapeDrawer.filledCircle(position.x + t.forward.x, position.y + t.forward.y, 1f, Color.BLUE)
-        shapeDrawer.filledCircle(position.x + t.up.x, position.y + t.up.y, 1f, Color.RED)
-        shapeDrawer.filledCircle(position.x + t.thePerp.x, position.y + t.thePerp.y, 1f, Color.GREEN)
+        if (thing is Bone)
+            drawBone(thing)
+        else {
+            val position = vec2(-100f)
+            shapeDrawer.setColor(Color.BLUE)
+            shapeDrawer.line(
+                position + thing.position.toIso(),
+                position + thing.position.toIso() + thing.forward.toIso(),
+                .5f
+            )
+            shapeDrawer.setColor(Color.RED)
+            shapeDrawer.line(
+                position + thing.position.toIso(),
+                position + thing.position.toIso() + thing.up.toIso(),
+                .5f
+            )
+            shapeDrawer.setColor(Color.GREEN)
+            shapeDrawer.line(
+                position + thing.position.toIso(),
+                position + thing.position.toIso() + thing.leftOrRight.toIso(),
+                .5f
+            )
+            shapeDrawer.setColor(Color.WHITE)
+            shapeDrawer.filledCircle(position + thing.position.toIso(), 1f, Color.WHITE)
+            shapeDrawer.filledCircle(position + thing.position.toIso() + thing.forward.toIso(), 1f, Color.BLUE)
+            shapeDrawer.filledCircle(position + thing.position.toIso() + thing.up.toIso(), 1f, Color.RED)
+            shapeDrawer.filledCircle(position + thing.position.toIso() + thing.leftOrRight.toIso(), 1f, Color.GREEN)
+        }
+    }
 
-        position.x += 50f
-        shapeDrawer.setColor(Color.BLUE)
-        shapeDrawer.line(position.x, position.y, position.x + t.forward.y, position.y + t.forward.z, .5f)
-        shapeDrawer.setColor(Color.RED)
-        shapeDrawer.line(position.x, position.y, position.x + t.up.y, position.y + t.up.z, .5f)
-        shapeDrawer.setColor(Color.GREEN)
-        shapeDrawer.line(position.x, position.y, position.x + t.thePerp.y, position.y + t.thePerp.z, .5f)
-        shapeDrawer.setColor(Color.WHITE)
-        shapeDrawer.filledCircle(position, 1f, Color.WHITE)
-        shapeDrawer.filledCircle(position.x + t.forward.y, position.y + t.forward.z, 1f, Color.BLUE)
-        shapeDrawer.filledCircle(position.x + t.up.y, position.y + t.up.z, 1f, Color.RED)
-        shapeDrawer.filledCircle(position.x + t.thePerp.y, position.y + t.thePerp.z, 1f, Color.GREEN)
+    private fun drawBone(bone: Bone) {
+        //drawOrientation(bone.position, bone.orientation)
+        shapeDrawer.setColor(Color.GRAY)
+        shapeDrawer.line(bone.position.toIso(), bone.boneEnd.toIso())
+        shapeDrawer.filledCircle(bone.position.toIso(), 1f, Color.GREEN)
+        shapeDrawer.filledCircle(bone.boneEnd.toIso(), 1f, Color.BLUE)
+    }
 
-        position.x += 50f
+    private fun drawOrientation(position: Vector3, orientation: Orientation) {
+        val up = orientation.up.cpy().scl(10f).toIso()
+        val forward = orientation.forward.cpy().scl(10f).toIso()
+        val left = orientation.leftOrRight.cpy().scl(10f).toIso()
+        val isoPos = position.toIso()
         shapeDrawer.setColor(Color.BLUE)
-        shapeDrawer.line(position.x, position.y, position.x + t.forward.z, position.y + t.forward.x, .5f)
+        shapeDrawer.line(isoPos, isoPos + forward, .5f)
         shapeDrawer.setColor(Color.RED)
-        shapeDrawer.line(position.x, position.y, position.x + t.up.z, position.y + t.up.x, .5f)
+        shapeDrawer.line(isoPos, isoPos + up, .5f)
         shapeDrawer.setColor(Color.GREEN)
-        shapeDrawer.line(position.x, position.y, position.x + t.thePerp.z, position.y + t.thePerp.x, .5f)
+        shapeDrawer.line(isoPos, isoPos + left, .5f)
         shapeDrawer.setColor(Color.WHITE)
-        shapeDrawer.filledCircle(position, 1f, Color.WHITE)
-        shapeDrawer.filledCircle(position.x + t.forward.z, position.y + t.forward.x, 1f, Color.BLUE)
-        shapeDrawer.filledCircle(position.x + t.up.z, position.y + t.up.x, 1f, Color.RED)
-        shapeDrawer.filledCircle(position.x + t.thePerp.z, position.y + t.thePerp.x, 1f, Color.GREEN)
+        shapeDrawer.filledCircle(isoPos, 1f, Color.WHITE)
+        shapeDrawer.filledCircle(isoPos + forward, 1f, Color.BLUE)
+        shapeDrawer.filledCircle(isoPos + up, 1f, Color.RED)
+        shapeDrawer.filledCircle(isoPos + left, 1f, Color.GREEN)
     }
 
     private fun drawSkeletonRecursive(bone3d: Bone3d) {
@@ -164,20 +278,20 @@ class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen
     private val normalCommandMap = command("Normal") {
         setBoth(Input.Keys.Z, "Zoom in", { zoom = 0f }, { zoom = 0.1f })
         setBoth(Input.Keys.X, "Zoom out", { zoom = 0f }, { zoom = -0.1f })
-//        setUp(Input.Keys.LEFT, "Previous bone") {
-//            boneList.previousItem()
-//            info { "Current bone: ${boneList.selectedItem.name}" }
-//        }
-//        setUp(Input.Keys.RIGHT, "Next bone") {
-//            boneList.nextItem()
-//            info { "Current bone: ${boneList.selectedItem.name}" }
-//        }
-        setBoth(Input.Keys.A, "Rotate Left", { rotationY = 0f }) { rotationY = 5f }
-        setBoth(Input.Keys.D, "Rotate Right", { rotationY = 0f }) { rotationY = -5f }
-        setBoth(Input.Keys.W, "Rotate X", { rotationX = 0f }) { rotationX = 5f }
-        setBoth(Input.Keys.S, "Rotate X", { rotationX = 0f }) { rotationX = -5f }
-        setBoth(Input.Keys.UP, "Rotate Z", { rotationZ = 0f }) { rotationZ = -5f }
-        setBoth(Input.Keys.DOWN, "Rotate Z", { rotationZ = 0f }) { rotationZ = -5f }
+        setUp(Input.Keys.LEFT, "Previous bone") {
+            thingList.previousItem()
+            info { "Current bone: ${thingList.selectedItem.name}" }
+        }
+        setUp(Input.Keys.RIGHT, "Next bone") {
+            thingList.nextItem()
+            info { "Current bone: ${thingList.selectedItem.name}" }
+        }
+        setBoth(Input.Keys.A, "Rotate Left", { aroundUp = 0f }) { aroundUp = 5f }
+        setBoth(Input.Keys.D, "Rotate Right", { aroundUp = 0f }) { aroundUp = -5f }
+        setBoth(Input.Keys.W, "Rotate X", { aroundLeft = 0f }) { aroundLeft = 5f }
+        setBoth(Input.Keys.S, "Rotate X", { aroundLeft = 0f }) { aroundLeft = -5f }
+        setBoth(Input.Keys.Q, "Rotate Z", { aroundForward = 0f }) { aroundForward = -5f }
+        setBoth(Input.Keys.E, "Rotate Z", { aroundForward = 0f }) { aroundForward = 5f }
     }
 
 
@@ -207,3 +321,45 @@ class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen
         mousePosition.set(screenMouse.x, screenMouse.y)
     }
 }
+
+/*
+
+        position.x += 50f
+        shapeDrawer.setColor(Color.BLUE)
+        shapeDrawer.line(position.x + thing.position.x, position.y, position.x + thing.position.x + thing.forward.x, position.y + thing.position.y + thing.forward.y, .5f)
+        shapeDrawer.setColor(Color.RED)
+        shapeDrawer.line(position.x + thing.position.x, position.y, position.x + thing.position.x + thing.up.x, position.y + thing.position.y + thing.up.y, .5f)
+        shapeDrawer.setColor(Color.GREEN)
+        shapeDrawer.line(position.x + thing.position.x, position.y, position.x + thing.position.x + thing.thePerp.x, position.y + thing.position.y + thing.thePerp.y, .5f)
+        shapeDrawer.setColor(Color.WHITE)
+        shapeDrawer.filledCircle(position.x + thing.position.x, position.y + thing.position.y, 1f, Color.WHITE)
+        shapeDrawer.filledCircle(position.x + thing.position.x + thing.forward.x, position.y + thing.position.y + thing.forward.y, 1f, Color.BLUE)
+        shapeDrawer.filledCircle(position.x + thing.position.x + thing.up.x, position.y + thing.position.y + thing.up.y, 1f, Color.RED)
+        shapeDrawer.filledCircle(position.x + thing.position.x + thing.thePerp.x, position.y + thing.position.y + thing.thePerp.y, 1f, Color.GREEN)
+
+        position.x += 50f
+        shapeDrawer.setColor(Color.BLUE)
+        shapeDrawer.line(position.x + thing.position.y, position.y + thing.position.z, position.x + thing.position.y + thing.forward.y, position.y + thing.position.z + thing.forward.z, .5f)
+        shapeDrawer.setColor(Color.RED)
+        shapeDrawer.line(position.x + thing.position.y, position.y + thing.position.z, position.x + thing.position.y + thing.up.y, position.y + thing.position.z + thing.up.z, .5f)
+        shapeDrawer.setColor(Color.GREEN)
+        shapeDrawer.line(position.x + thing.position.y, position.y + thing.position.z, position.x + thing.position.y + thing.thePerp.y, position.y + thing.position.z + thing.thePerp.z, .5f)
+        shapeDrawer.setColor(Color.WHITE)
+        shapeDrawer.filledCircle(position.x + thing.position.y, position.y + thing.position.z, 1f, Color.WHITE)
+        shapeDrawer.filledCircle(position.x + thing.position.y + thing.forward.y, position.y + thing.position.z + thing.forward.z, 1f, Color.BLUE)
+        shapeDrawer.filledCircle(position.x + thing.position.y + thing.up.y, position.y + thing.position.z + thing.up.z, 1f, Color.RED)
+        shapeDrawer.filledCircle(position.x + thing.position.y + thing.thePerp.y, position.y + thing.position.z + thing.thePerp.z, 1f, Color.GREEN)
+
+        position.x += 50f
+        shapeDrawer.setColor(Color.BLUE)
+        shapeDrawer.line(position.x, position.y, position.x + thing.position.z + thing.forward.z, position.y + thing.position.x + thing.forward.x, .5f)
+        shapeDrawer.setColor(Color.RED)
+        shapeDrawer.line(position.x, position.y, position.x + thing.position.z + thing.up.z, position.y + thing.position.x + thing.up.x, .5f)
+        shapeDrawer.setColor(Color.GREEN)
+        shapeDrawer.line(position.x, position.y, position.x + thing.position.z + thing.thePerp.z, position.y + thing.position.x + thing.thePerp.x, .5f)
+        shapeDrawer.setColor(Color.WHITE)
+        shapeDrawer.filledCircle(position.x + thing.position.z, position.x + thing.position.x, 1f, Color.WHITE)
+        shapeDrawer.filledCircle(position.x + thing.position.z + thing.forward.z, position.y + thing.position.x + thing.forward.x, 1f, Color.BLUE)
+        shapeDrawer.filledCircle(position.x + thing.position.z + thing.up.z, position.y + thing.position.x + thing.up.x, 1f, Color.RED)
+        shapeDrawer.filledCircle(position.x + thing.position.z + thing.thePerp.z, position.y + thing.position.x + thing.thePerp.x, 1f, Color.GREEN)
+ */
