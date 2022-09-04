71b70330a5b28f630063ece0ad8384076cd0030d
Tommie Nygren <tommie.nygren@gmail.com>
2022-09-04 22:44:09 +0200

    Whatever it is I have now, I find it cleaner than the previous code.
    But what does it do that actually differs? dunno

--- core/src/main/kotlin/screens/concepts/ConceptScreen.kt
+++ core/src/main/kotlin/screens/concepts/ConceptScreen.kt
@@ -4,27 +4,18 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.Input.Buttons
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.MathUtils
-import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.viewport.ExtendViewport
-import data.selectedItemListOf
+import eater.core.selectedItemListOf
 import gamestate.GameEvent
 import gamestate.GameState
 import ktx.graphics.use
 import ktx.log.info
-import ktx.math.minus
-import ktx.math.plus
 import ktx.math.vec2
 import ktx.math.vec3
 import screens.BasicScreen
 import screens.command
-import screens.stuff.AnimatedSprited3d
-import screens.stuff.Bone3d
-import screens.stuff.selectRecursive
-import screens.stuff.toIso
 import screens.ui.KeyPress
-import space.earlygrey.shapedrawer.ShapeDrawer
 import statemachine.StateMachine
 import tru.Assets
 
@@ -34,15 +25,23 @@ class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen
     override val viewport = ExtendViewport(200f, 180f)
 
     var zoom = 0f
-    var rotX = 0f
+    var rotRight = 0f
+    var rotUp = 0f
+    var rotForward = 0f
     var rotY = 0f
+    var rotX = 0f
     var rotZ = 0f
+    var modifier = 1f
     var elapsedTime = 0f
     private val shapeDrawer by lazy { Assets.shapeDrawer }
 
-    val node = Node("base", vec3(), 0f, 0f, 0f)
+    val node = Node("base", vec3(), 0f, 0f, 0f).apply {
+        addChild(Node("other thing", vec3(100f,0f,0f), 15f, 15f, 15f).apply {
+            addChild(Segment("a segmebt", vec3(0f, 50f, 0f),100f, Direction3d(25f, 0f,0f),Color.WHITE))
+        })
+    }
 
-//    val thingList = selectedItemListOf(*t.allThings.values.toTypedArray())
+    val thingList = selectedItemListOf(*node.flatChildren.values.toTypedArray())
 
     override fun render(delta: Float) {
         elapsedTime += delta
@@ -52,15 +51,21 @@ class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen
         camera.zoom = camera.zoom + 0.05f * zoom
         super.render(delta)
 
-//        if (!MathUtils.isZero(rotY))
-//            thingList.selectedItem.rotateAgainstJoint(rotY)
-//
-//        if (!MathUtils.isZero(rotX))
-//            thingList.selectedItem.rotateAroundParentForward(rotX)
-//
-//        if (!MathUtils.isZero(rotZ))
-//            thingList.selectedItem.rotateAroundParentUp(rotZ)
+        if (!MathUtils.isZero(rotUp))
+            thingList.selectedItem.rotateAroundUp(rotUp)
 
+        if (!MathUtils.isZero(rotRight))
+            thingList.selectedItem.rotateAroundRight(rotRight)
+
+        if (!MathUtils.isZero(rotForward))
+            thingList.selectedItem.rotateAroundForward(rotForward)
+
+        if (!MathUtils.isZero(rotX))
+            thingList.selectedItem.rotateAroundX(rotX)
+        if (!MathUtils.isZero(rotY))
+            thingList.selectedItem.rotateAroundY(rotY)
+        if (!MathUtils.isZero(rotZ))
+            thingList.selectedItem.rotateAroundZ(rotZ)
 
         batch.use {
             shapeDrawer.filledCircle(mousePosition, 1.5f, Color.RED)
@@ -73,20 +78,21 @@ class ConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen
     private val normalCommandMap = command("Normal") {
         setBoth(Input.Keys.Z, "Zoom in", { zoom = 0f }, { zoom = 0.1f })
         setBoth(Input.Keys.X, "Zoom out", { zoom = 0f }, { zoom = -0.1f })
-//        setUp(Input.Keys.LEFT, "Previous bone") {
-//            thingList.previousItem()
-//            info { "Current bone: ${thingList.selectedItem.name}" }
-//        }
-//        setUp(Input.Keys.RIGHT, "Next bone") {
-//            thingList.nextItem()
-//            info { "Current bone: ${thingList.selectedItem.name}" }
-//        }
-        setBoth(Input.Keys.A, "Rotate Left", { rotX = 0f }) { rotX = 5f }
-        setBoth(Input.Keys.D, "Rotate Right", { rotX = 0f }) { rotX = -5f }
-        setBoth(Input.Keys.W, "Rotate X", { rotY = 0f }) { rotY = 5f }
-        setBoth(Input.Keys.S, "Rotate X", { rotY = 0f }) { rotY = -5f }
-        setBoth(Input.Keys.Q, "Rotate Z", { rotZ = 0f }) { rotZ = -5f }
-        setBoth(Input.Keys.E, "Rotate Z", { rotZ = 0f }) { rotZ = 5f }
+        setUp(Input.Keys.LEFT, "Previous bone") {
+            thingList.previousItem()
+            info { "Current bone: ${thingList.selectedItem.name}" }
+        }
+        setUp(Input.Keys.RIGHT, "Next bone") {
+            thingList.nextItem()
+            info { "Current bone: ${thingList.selectedItem.name}" }
+        }
+        setBoth(Input.Keys.A, "Rotate Left", { rotRight = 0f }) { rotRight = 5f * modifier }
+        setBoth(Input.Keys.D, "Rotate Right", { rotX = 0f }) { rotX = 5f * modifier }
+        setBoth(Input.Keys.W, "Rotate X", { rotUp = 0f }) { rotUp = 5f * modifier }
+        setBoth(Input.Keys.S, "Rotate X", { rotY = 0f }) { rotY = 5f * modifier }
+        setBoth(Input.Keys.Q, "Rotate Z", { rotForward = 0f }) { rotForward = 5f * modifier }
+        setBoth(Input.Keys.E, "Rotate Z", { rotZ = 0f }) { rotZ = 5f * modifier }
+        setBoth(Input.Keys.SHIFT_RIGHT, "inverse input", { modifier = 1f}) { modifier = -1f}
     }
 
 
--- core/src/main/kotlin/screens/concepts/Node.kt
+++ core/src/main/kotlin/screens/concepts/Node.kt
@@ -2,10 +2,14 @@ package screens.concepts
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.Quaternion
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.math.Vector3
 import isometric.toIsoFrom3d
 import ktx.math.plus
+import ktx.math.unaryMinus
 import ktx.math.vec2
+import ktx.math.vec3
+import screens.stuff.selectRecursive
 import space.earlygrey.shapedrawer.ShapeDrawer
 
 /**
@@ -29,19 +33,69 @@ import space.earlygrey.shapedrawer.ShapeDrawer
  * Perhaps.
  */
 
-open class Node(var name: String, val localPosition: Vector3, var yaw: Float, var pitch: Float, var roll: Float, var color: Color = Color.RED) {
+open class Direction3d(yaw: Float = 0f, pitch: Float = 0f, roll: Float = 0f) {
     val forward = Vector3(0f, 0f, -1f)
     val up = Vector3(0f, 1f, 0f)
     val right = Vector3(-1f, 0f, 0f)
+    val left get() = -right
+    val down get() = -up
+    val back get() = -forward
+    init {
+        val initialRotation = Quaternion().setEulerAngles(yaw, pitch, roll) //Might work?
+        rotate(initialRotation)
+    }
+    fun rotate(q: Quaternion) {
+        q.transform(forward)
+        q.transform(up)
+        q.transform(right)
+    }
+    private val destination = vec2()
+
+    fun renderIso(origin: Vector2, shapeDrawer: ShapeDrawer, scale: Float) {
+        destination.toIsoFrom3d(forward).scl(scale)
+        shapeDrawer.setColor(Color.BLUE)
+        shapeDrawer.line(origin, origin + destination)
+        shapeDrawer.filledCircle(origin + destination, 1f)
+        destination.toIsoFrom3d(up).scl(scale)
+        shapeDrawer.setColor(Color.GREEN)
+        shapeDrawer.line(origin, origin + destination)
+        shapeDrawer.filledCircle(origin + destination, 1f)
+        destination.toIsoFrom3d(right).scl(scale)
+        shapeDrawer.setColor(Color.RED)
+        shapeDrawer.line(origin, origin + destination)
+        shapeDrawer.filledCircle(origin + destination, 1f)
+    }
+}
+
+open class Segment(name: String, localPosition: Vector3, var length:Float, val boneDirection3d: Direction3d, color: Color): Node(name, localPosition, color = color) {
+    var boneEnd: Vector3 = vec3()
+        private set
+    get() {
+        field.set(boneDirection3d.forward).scl(length)
+        return field
+    }
+    override fun renderIso(shapeDrawer: ShapeDrawer, scale: Float) {
+        super.renderIso(shapeDrawer, scale)
+        origin.toIsoFrom3d(position)
+        boneDirection3d.renderIso(origin, shapeDrawer, scale)
+        destination.toIsoFrom3d(position + boneEnd)
+        shapeDrawer.line(origin, destination)
+    }
+
+    override fun rotate(q: Quaternion) {
+        super.rotate(q)
+        boneDirection3d.rotate(q)
+    }
+}
+
+open class Node(var name: String, val localPosition: Vector3, yaw: Float = 0f, pitch: Float = 0f, roll: Float = 0f, var color: Color = Color.RED) {
+    val direction = Direction3d(yaw, pitch, roll)
+    val forward get() = direction.forward
+    val up get() = direction.up
+    val right get() = direction.right
     val children = mutableSetOf<Node>()
     var parent: Node? = null
     val position: Vector3 get() = if(parent == null) localPosition else parent!!.position + localPosition
-    init {
-        val initialRotation = Quaternion().setEulerAngles(yaw, pitch, roll) //Might work?
-        initialRotation.transform(forward)
-        initialRotation.transform(up)
-        initialRotation.transform(right)
-    }
     fun addChild(child: Node) {
         child.parent = this
         children.add(child)
@@ -55,21 +109,55 @@ open class Node(var name: String, val localPosition: Vector3, var yaw: Float, va
 
     val origin = vec2()
     val destination = vec2()
-    fun renderIso(shapeDrawer: ShapeDrawer) {
+    open fun renderIso(shapeDrawer: ShapeDrawer, scale: Float = 40f) {
+        for(child in children)
+            child.renderIso(shapeDrawer)
+
         origin.toIsoFrom3d(position)
         shapeDrawer.setColor(color)
         shapeDrawer.filledCircle(origin, 1f)
-        destination.toIsoFrom3d(forward).scl(10f)
-        shapeDrawer.setColor(Color.BLUE)
-        shapeDrawer.line(origin, origin + destination)
-        shapeDrawer.filledCircle(origin + destination, 1f)
-        destination.toIsoFrom3d(up).scl(10f)
-        shapeDrawer.setColor(Color.GREEN)
-        shapeDrawer.line(origin, origin + destination)
-        shapeDrawer.filledCircle(origin + destination, 1f)
-        destination.toIsoFrom3d(right).scl(10f)
-        shapeDrawer.setColor(Color.RED)
-        shapeDrawer.line(origin, origin + destination)
-        shapeDrawer.filledCircle(origin + destination, 1f)
+        direction.renderIso(origin, shapeDrawer, scale)
+    }
+
+    fun rotateAroundUp(degrees: Float) {
+        val q = Quaternion(up, degrees)
+        rotate(q)
+    }
+    fun rotateAroundRight(degrees: Float) {
+        val q = Quaternion(right, degrees)
+        rotate(q)
+    }
+    fun rotateAroundForward(degrees: Float) {
+        val q = Quaternion(forward, degrees)
+        rotate(q)
+    }
+    fun rotateAroundY(degrees: Float) {
+        val q = Quaternion(Vector3.Y, degrees)
+        rotate(q)
+    }
+    fun rotateAroundX(degrees: Float) {
+        val q = Quaternion(Vector3.X, degrees)
+        rotate(q)
+    }
+    fun rotateAroundZ(degrees: Float) {
+        val q = Quaternion(Vector3.Z, degrees)
+        rotate(q)
+    }
+
+    open fun rotate(q: Quaternion) {
+        direction.rotate(q)
+        q.transform(localPosition)
+        for(child in children) {
+            child.rotate(q)
+        }
+    }
+
+    val flatChildren get() = toMap()
+
+    open fun toMap(): Map<String, Node> {
+        return listOf(
+            this,
+            *children.asSequence().selectRecursive { children.asSequence() }.toList().toTypedArray()
+        ).associateBy { it.name }
     }
 }
