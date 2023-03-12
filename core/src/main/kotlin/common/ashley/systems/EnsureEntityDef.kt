da523134050c7d9c8339ed8d142c6de5ec198f3f
Tommie Nygren <tommie.nygren@gmail.com>
2023-03-12 16:22:10 +0100

    Made it work again, but without submodule, but with the common stuff in a separate project

--- core/src/main/kotlin/common/ashley/systems/EnsureEntityDef.kt
+++
@@ -1,7 +0,0 @@
-package common.ashley.systems
-
-import com.badlogic.ashley.core.Family
-
-class EnsureEntityDef(val entityFamily: Family, val numberOfEntities: Int, val interval:Float = 1f, val random: Boolean, val creator: ()->Unit) {
-    var coolDown = interval
-}
--- core/src/main/kotlin/common/world/Tile.kt
+++
@@ -1,7 +0,0 @@
-package common.world
-
-interface Tile {
-    val neighbours: MutableList<Tile>
-    val x: Int
-    val y: Int
-}
