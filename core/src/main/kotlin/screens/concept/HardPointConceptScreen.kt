304ea0398ad3c02e26c7b5d5219a24c6dfa28f0d
Tommie Nygren <tommie.nygren@gmail.com>
2023-04-10 12:02:26 +0200

    WIP on hard points

--- core/src/main/kotlin/screens/concept/HardPointConceptScreen.kt
+++ core/src/main/kotlin/screens/concept/HardPointConceptScreen.kt
@@ -1,26 +1,31 @@
 package screens.concept
 
-import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.math.Interpolation
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import gamestate.GameEvent
 import gamestate.GameState
+import ktx.graphics.use
+import ktx.math.plus
+import ktx.math.times
 import ktx.math.vec2
-import ktx.scene2d.actors
-import ktx.scene2d.dialog
-import ktx.scene2d.label
-import ktx.scene2d.scene2d
 import screens.basic.BasicScreen
 import screens.command.command
 import screens.ui.KeyPress
 import statemachine.StateMachine
+import tru.AnimState
 import tru.Assets
 import tru.CardinalDirection
-import ui.CrawlDialog
 
-class HardPointConceptScreen(gameState: StateMachine<GameState, GameEvent>) : BasicScreen(gameState) {
+class HardPointConceptScreen(
+    gameState: StateMachine<GameState, GameEvent>
+) : BasicScreen(gameState) {
+    private val character = Character().apply {
+        worldPosition.set(200f, 200f)
+    }
+    private val characterAnim = Assets.characterTurboAnims.first { it.key == "boy" }
+    private var stateTime = 0f
 
     private val normalCommandMap = command("Normal") {
 //        setUp(Input.Keys.SPACE, "Show the scrolling dialog") {
@@ -70,18 +75,30 @@ class HardPointConceptScreen(gameState: StateMachine<GameState, GameEvent>) : Ba
     }
 
     override fun render(delta: Float) {
+        camera.position.set(character.worldPosition.x, character.worldPosition.y, 0f)
         super.render(delta)
         stage.act()
         stage.draw()
+        renderInternal(delta)
     }
 
     fun renderInternal(delta: Float) {
-
+        stateTime += delta
+        batch.use {
+            val region =
+                characterAnim.animations[AnimState.Idle]!!.animations[character.cardinalDirection]!!.getKeyFrame(
+                    stateTime
+                )
+            it.draw(region, character.worldPosition.x, character.worldPosition.y)
+            for ((key, point) in character.worldAnchors) {
+                shapeDrawer.filledCircle(point, 5f, if(key.contains("left")) Color.RED else Color.GREEN)
+            }
+        }
     }
 }
 
 object CardinalToAngles {
-    val cardinals = mapOf(
+    private val cardinals = mapOf(
         241f..300f to CardinalDirection.South,
         301f..360f to CardinalDirection.East,
         0f..60f to CardinalDirection.East,
@@ -89,21 +106,30 @@ object CardinalToAngles {
         121f..240f to CardinalDirection.West
     )
 
-    fun angleToCardinal(angle:Float): CardinalDirection {
+    fun angleToCardinal(angle: Float): CardinalDirection {
         val dirKey = cardinals.keys.firstOrNull { it.contains(angle) }
-        return if(dirKey != null) cardinals[dirKey]!! else CardinalDirection.South
+        return if (dirKey != null) cardinals[dirKey]!! else CardinalDirection.South
     }
 }
 
 class Character {
-    var direction: CardinalDirection = CardinalDirection.South
     var worldPosition = vec2()
-    val worldDirection = vec2(0f,-1f)
+    val worldDirection = vec2(1f, 0f)
+    val angleDegrees get() = worldDirection.angleDeg()
+    val cardinalDirection get() = CardinalToAngles.angleToCardinal(angleDegrees)
     var width = 32f
     var height = 32f
     var scale = 1.0f
     val center = Vector2.Zero.cpy()
-    val rightShoulderLocal = vec2(-0.5f, 0f)
-    val leftShoulderLocal = vec2(0.5f, 0f)
+
+    val anchors = mapOf(
+        "rightshoulder" to vec2(0f, -0.25f),
+        "leftshoulder" to vec2(0f, 0.25f)
+    )
+
+    //the angle should be the cardinal directions angle for the anchor points - they are static!
+    val worldAnchors get() = anchors.map {
+        it.key to worldPosition + it.value * 32f
+    }.toMap()
 
 }
