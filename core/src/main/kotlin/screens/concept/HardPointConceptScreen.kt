d14ebb6fc277500b0e5e26b0014f5a2e2c397b2a
Tommie Nygren <tommie.nygren@gmail.com>
2023-04-11 18:26:13 +0200

    We have done it

--- core/src/main/kotlin/screens/concept/HardPointConceptScreen.kt
+++ core/src/main/kotlin/screens/concept/HardPointConceptScreen.kt
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Polygon
 import com.badlogic.gdx.math.Vector2
-import extensions.angleTo
 import gamestate.GameEvent
 import gamestate.GameState
 import ktx.graphics.use
@@ -77,8 +76,8 @@ class HardPointConceptScreen(
 
     override fun show() {
         super.show()
-        viewport.worldWidth = 800f
-        viewport.worldHeight = 600f
+        viewport.worldWidth = 200f
+        viewport.worldHeight = 150f
         camera.setToOrtho(false)
     }
 
@@ -95,10 +94,10 @@ class HardPointConceptScreen(
     }
 
     private val drawMethods = mapOf(
-        CardinalDirection.East to listOf(::drawRegion, ::drawRifle),
-        CardinalDirection.South to listOf(::drawRegion, ::drawRifle),
-        CardinalDirection.West to listOf(::drawRifle, ::drawRegion),
-        CardinalDirection.North to listOf(::drawRifle, ::drawRegion)
+        CardinalDirection.East to listOf(::drawLeftHand, ::drawRegion, ::drawRifle, ::drawRightHand),
+        CardinalDirection.South to listOf(::drawRegion, ::drawLeftHand, ::drawRightHand, ::drawRifle),
+        CardinalDirection.West to listOf(::drawRightHand, ::drawRifle, ::drawRegion, ::drawLeftHand),
+        CardinalDirection.North to listOf(::drawLeftHand, ::drawRightHand, ::drawRifle, ::drawRegion)
     )
 
     private fun drawRegion(character: Character) {
@@ -113,14 +112,40 @@ class HardPointConceptScreen(
         )
     }
 
+    fun drawAnchors(character: Character) {
+        for ((key, point) in character.worldAnchors) {
+            shapeDrawer.filledCircle(
+                point,
+                5f,
+                if (key.contains("left")) Color.RED else Color.GREEN
+            )
+        }
+    }
+
     private fun renderInternal(delta: Float) {
         stateTime += delta
         batch.use {
-            drawRegion(character)
-            drawRifle(character)
-//            for (drawMethod in drawMethods[character.cardinalDirection]!!)
-//                drawMethod(character)
+            for (drawMethod in drawMethods[character.cardinalDirection]!!)
+                drawMethod(character)
+
         }
+    }
+
+    private fun renderAimVector() {
+        shapeDrawer.filledCircle(character.worldPosition + (character.aimVector.cpy().scl(25f)), 2.5f, Color.YELLOW)
+    }
+
+    private fun renderLineToMouse() {
+        val start = character.worldPosition + (character.aimVector.cpy().scl(25f))
+        val stop = MousePosition.worldPosition2D.cpy()
+        shapeDrawer.line(start, stop, Color.YELLOW)
+        shapeDrawer.filledCircle(start, 2f, Color.GREEN)
+        shapeDrawer.filledCircle(stop, 2f, Color.RED)
+        shapeDrawer.filledCircle(MousePosition.worldPosition2D, 5f, Color.WHITE)
+
+        start.set(character.worldAnchors["rightshoulder"]!!)
+
+        shapeDrawer.line(start, stop, Color.RED)
     }
 
     private val rifle = Polygon(floatArrayOf(0f, 2f, 50f, 2f, 50f, -2f, 0f, -2f))
@@ -132,32 +157,20 @@ class HardPointConceptScreen(
         val scaleX = MathUtils.lerp(0.5f, 1f, character.aimVector.x.absoluteValue)
         rifle.setScale(scaleX, 1f)
 
-//        shapeDrawer.setColor(Color.GRAY)
-//        shapeDrawer.filledPolygon(rifle)
-//        shapeDrawer.setColor(Color.WHITE)
-        drawHandsAndArms(character)
+        shapeDrawer.setColor(Color.GRAY)
+        shapeDrawer.filledPolygon(rifle)
+        shapeDrawer.setColor(Color.WHITE)
     }
 
     private val skinColor = Color(0.8f, 0.6f, 0.5f, 1f)
 
-    private fun drawHandsAndArms(character: Character) {
+    fun drawLeftHand(character: Character) {
         val rightShoulder = character.worldAnchors["rightshoulder"]!!.cpy()
         val leftShoulder = character.worldAnchors["leftshoulder"]!!.cpy()
-        val rightGripLength = 5f
-        val rightHandDirection = character.aimVector.cpy().scl(rightGripLength)
-        shapeDrawer.filledCircle(rightShoulder + rightHandDirection, 4f, skinColor)
-        val upperArmLength = 5f
-        val lowerArmLength = 8f
-
-        val alpha =
-            MathUtils.acos((rightGripLength.pow(2) + upperArmLength.pow(2) - lowerArmLength.pow(2)) / (2 * upperArmLength * rightGripLength))
-        val rightUpperArmVector =
-            vec2(1f, 0f).setAngleDeg(rightHandDirection.angleDeg()).rotateRad(-alpha).scl(upperArmLength)
-        shapeDrawer.line(rightShoulder, rightShoulder + rightUpperArmVector, Color.GREEN, 3f)
-        shapeDrawer.line(rightShoulder + rightUpperArmVector, rightShoulder + rightHandDirection, Color.YELLOW, 3f)
-
-        val leftGripLength = 15f
-        val leftHandDirection = character.aimVector.cpy().scl(leftGripLength)
+        val lowerArmLength = 24f
+        val upperArmLength = lowerArmLength * 5f / 8f
+        val leftGripLength = 20f
+        val leftHandDirection = character.aimVector.cpy().scl(leftGripLength).scl(MathUtils.lerp(0.5f, 1f, character.aimVector.x.absoluteValue))
         val leftHandGripPoint = rightShoulder + leftHandDirection
         shapeDrawer.filledCircle(leftHandGripPoint, 4f, skinColor)
         leftHandDirection.set(leftHandGripPoint).sub(leftShoulder).nor()
@@ -165,19 +178,40 @@ class HardPointConceptScreen(
 
         val beta =
             MathUtils.acos((leftDistance.pow(2) + upperArmLength.pow(2) - lowerArmLength.pow(2)) / (2 * leftDistance * upperArmLength))
-        val leftUpperArmVector = leftHandDirection.cpy().rotateRad(beta).scl(upperArmLength)
-        shapeDrawer.line(leftShoulder, leftShoulder + leftUpperArmVector, Color.RED, 3f)
-        shapeDrawer.line(leftShoulder + leftUpperArmVector, leftHandGripPoint, Color.PINK, 3f)
+        val leftUpperArmVector = leftHandDirection.cpy().rotateRad(beta).scl(upperArmLength).scl(MathUtils.lerp(0.5f, 1f, character.aimVector.x.absoluteValue))
+        shapeDrawer.line(leftShoulder, leftShoulder + leftUpperArmVector, Color.BROWN, 6f)
+        shapeDrawer.line(leftShoulder + leftUpperArmVector, leftHandGripPoint, Color.BROWN, 6f)
     }
+
+    fun drawRightHand(character: Character) {
+        val rightShoulder = character.worldAnchors["rightshoulder"]!!.cpy()
+        val rightGripLength = 8f
+        val rightHandDirection = character.aimVector.cpy().scl(rightGripLength).scl(MathUtils.lerp(0.5f, 1f, character.aimVector.x.absoluteValue))
+        val rightHandGripPoint = rightShoulder + rightHandDirection
+        shapeDrawer.filledCircle(rightHandGripPoint, 4f, skinColor)
+
+        val lowerArmLength = 12f
+        val upperArmLength = lowerArmLength * 5f / 8f
+        rightHandDirection.set(rightHandGripPoint).sub(rightShoulder).nor()
+        val rightDistance = rightHandGripPoint.dst(rightShoulder)
+
+        val beta =
+            MathUtils.acos((rightDistance.pow(2) + upperArmLength.pow(2) - lowerArmLength.pow(2)) / (2 * rightDistance * upperArmLength))
+
+        val rightUpperArmVector = rightHandDirection.cpy().rotateRad(-beta).scl(upperArmLength).scl(MathUtils.lerp(0.5f, 1f, character.aimVector.x.absoluteValue))
+        shapeDrawer.line(rightShoulder, rightShoulder + rightUpperArmVector, Color.BROWN, 6f)
+        shapeDrawer.line(rightShoulder + rightUpperArmVector, rightHandGripPoint, Color.BROWN, 6f)
+    }
+
 }
 
 object CardinalToAngles {
     val cardinals = mapOf(
-        241f..300f to CardinalDirection.South,
-        301f..360f to CardinalDirection.East,
-        0f..60f to CardinalDirection.East,
-        61f..120f to CardinalDirection.North,
-        121f..240f to CardinalDirection.West
+        221f..320f to CardinalDirection.South,
+        321f..360f to CardinalDirection.East,
+        0f..40f to CardinalDirection.East,
+        41f..140f to CardinalDirection.North,
+        141f..220f to CardinalDirection.West
     )
 
     val cardinalAbsoluteAngles = mapOf(
@@ -213,15 +247,33 @@ class Character {
     val center = Vector2.Zero.cpy()
 
     private val anchors = mapOf(
-        "rightshoulder" to vec2(0.1f, 0.5f),
-        "leftshoulder" to vec2(0.1f, -0.5f)
+        CardinalDirection.East to
+                mapOf(
+                    "rightshoulder" to vec2(-0.25f, 0.5f),
+                    "leftshoulder" to vec2(0f, -0.25f)
+                ),
+        CardinalDirection.South to
+                mapOf(
+                    "rightshoulder" to vec2(0.25f, 0.5f),
+                    "leftshoulder" to vec2(0.25f, -0.5f)
+                ),
+        CardinalDirection.West to
+                mapOf(
+                    "rightshoulder" to vec2(0f, 0.25f),
+                    "leftshoulder" to vec2(-0.25f, -0.5f)
+                ),
+        CardinalDirection.North to
+                mapOf(
+                    "rightshoulder" to vec2(-0.25f, 0.5f),
+                    "leftshoulder" to vec2(-0.25f, -0.5f)
+                )
     )
 
-    var aimVector = Vector2.X.cpy()
+    val aimVector = Vector2.X.cpy()
 
     //the angle should be the cardinal directions angle for the anchor points - they are static!
     val worldAnchors
-        get() = anchors.map {
+        get() = anchors[cardinalDirection]!!.map {
             it.key to worldPosition + vec2().set(it.value.x, it.value.y * 0.5f).times(32f)
                 .setAngleDeg(direction.cardinalAngle - it.value.angleDeg())
         }.toMap()
