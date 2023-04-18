999f84f16d358d1e32c2fb700537d25a4630cc5c
Tommie Nygren <tommie.nygren@gmail.com>
2023-04-18 14:24:53 +0200

    Going deeper into the weeds.

--- core/src/main/kotlin/ecs/components/graphics/RenderableType.kt
+++ core/src/main/kotlin/ecs/components/graphics/RenderableType.kt
@@ -15,7 +15,6 @@ import eater.physics.addComponent
 import ecs.components.gameplay.DestroyComponent
 import ecs.components.player.PlayerControlComponent
 import ktx.math.plus
-import ktx.math.times
 import physics.*
 import space.earlygrey.shapedrawer.ShapeDrawer
 import kotlin.math.absoluteValue
@@ -123,7 +122,7 @@ sealed class RenderableType {
         private val skinColor = Color(0.8f, 0.6f, 0.5f, 1f)
         private val rifle = Polygon(floatArrayOf(0f, 2f, 50f, 2f, 50f, -2f, 0f, -2f))
         private var drawRifleAndArms = true
-        private var drawDebug = false
+        private var drawDebug = true
         private var aimX = 0f
         private var aimY = 0f
 
@@ -136,7 +135,7 @@ sealed class RenderableType {
             }
         }
 
-        fun drawAnchors(character: CharacterComponent) {
+        private fun drawAnchors(character: CharacterComponent) {
             for ((key, point) in character.worldAnchors) {
                 shapeDrawer.filledCircle(
                     point,
@@ -190,10 +189,10 @@ sealed class RenderableType {
         }
 
         private val drawMethods: Map<CardinalDirection, List<(Entity, CharacterComponent) -> Unit>> = mapOf(
-            CardinalDirection.East to listOf(::drawLeftHand, ::drawRegion, ::drawRifle, ::drawRightHand),
-            CardinalDirection.South to listOf(::drawRegion, ::drawLeftHand, ::drawRightHand, ::drawRifle),
-            CardinalDirection.West to listOf(::drawRightHand, ::drawRifle, ::drawRegion, ::drawLeftHand),
-            CardinalDirection.North to listOf(::drawLeftHand, ::drawRightHand, ::drawRifle, ::drawRegion)
+            CardinalDirection.East to listOf(::drawLeftArm, ::drawTorso, ::drawRifle, ::drawRightHand),
+            CardinalDirection.South to listOf(::drawTorso, ::drawLeftArm, ::drawRightHand, ::drawRifle),
+            CardinalDirection.West to listOf(::drawRightHand, ::drawRifle, ::drawTorso, ::drawLeftArm),
+            CardinalDirection.North to listOf(::drawLeftArm, ::drawRightHand, ::drawRifle, ::drawTorso)
         )
 
         private fun drawRegion(entity: Entity, character: CharacterComponent) {
@@ -228,7 +227,77 @@ sealed class RenderableType {
             }
         }
 
-        private fun drawLeftHand(entity: Entity, character: CharacterComponent) {
+        /**
+         * The torso could very well consist of drawing FOUR polygons
+         */
+        private fun drawTorso(entity: Entity, character: CharacterComponent) {
+            /**
+             * The torso is drawn as a polygon that changes as we go about our day.
+             * The points defining the polygon are
+             *
+             * The right shoulder
+             * The left shoulder
+             * The right hip
+             * The left hip
+             */
+            val rightShoulder = character.worldAnchors["rightshoulder"]!!.cpy()
+            val leftShoulder = character.worldAnchors["leftshoulder"]!!.cpy()
+            val rightHip = rightShoulder.cpy().sub(0f, 5f)
+            val leftHip = leftShoulder.cpy().sub(0f, 5f)
+
+            val rsf = character.worldAnchors["rightsf"]!!.cpy()
+            val lsf = character.worldAnchors["leftsf"]!!.cpy()
+
+            shapeDrawer.setColor(Color.BLUE)
+            shapeDrawer.filledPolygon(
+                floatArrayOf(
+                    rightShoulder.x,
+                    rightShoulder.y,
+                    leftShoulder.x,
+                    leftShoulder.y,
+                    leftHip.x,
+                    leftHip.y,
+                    rightHip.x,
+                    rightHip.y
+                )
+            )
+            shapeDrawer.setColor(Color.CYAN)
+            shapeDrawer.filledPolygon(
+                floatArrayOf(
+                    rightShoulder.x,
+                    rightShoulder.y,
+                    leftShoulder.x,
+                    leftShoulder.y,
+                    lsf.x,
+                    lsf.y,
+                    rsf.x,
+                    rsf.y
+                )
+            )
+//            rightShoulder
+//            leftShoulder
+//            rightHip
+//            leftHip
+
+//            shapeDrawer.setColor(Color.WHITE)
+        }
+
+        private fun drawLeftArm(entity: Entity, character: CharacterComponent) {
+            /**
+             * What is a leftarm?
+             *
+             * It is a bunch of points that are defined somehow
+             * that we then want to draw lines and circles in relation to
+             *
+             * So, the left arm consists of:
+             *
+             * The Right Shoulder: a vector
+             * The Left Shoulder: a vector
+             * An Aim Vector
+             * A Grip Point - defined as a distance N from the right shoulder in the direction of the aim vector
+             * An upper arm - defined as a unit vector pointing from shoulder to grip point, rotated by angle Beta and scaled by the length of the upper arm
+             *
+             */
             if (drawRifleAndArms) {
                 val rightShoulder = character.worldAnchors["rightshoulder"]!!.cpy()
                 val leftShoulder = character.worldAnchors["leftshoulder"]!!.cpy()
@@ -253,9 +322,16 @@ sealed class RenderableType {
                         )
                 val leftHandGripPoint = (rightShoulder + leftHandDirection)
                 shapeDrawer.filledCircle(leftHandGripPoint, 4f * character.scale, skinColor)
-                leftHandDirection.set(leftHandGripPoint).sub(leftShoulder).nor()
+                // set the direction to a unit vector pointing FROM the shoulder TO the hand
+                leftHandDirection
+                    .set(leftHandGripPoint)
+                    .sub(leftShoulder)
+                    .nor()
+
+                // calculate distance from shoulder to hand
                 val leftDistance = leftHandGripPoint.dst(leftShoulder)
 
+                // calculate angle between shoulder and upper arm
                 val beta =
                     MathUtils.acos(
                         (
@@ -276,11 +352,16 @@ sealed class RenderableType {
                                 1f,
                                 character
                                     .aimVector
-                                    .x
+                                    .y
                                     .absoluteValue
                             )
                         )
-                shapeDrawer.line(leftShoulder, leftShoulder + leftUpperArmVector, Color.BROWN, 6f * character.scale)
+                shapeDrawer.line(
+                    leftShoulder,
+                    leftShoulder + leftUpperArmVector,
+                    Color.BROWN,
+                    6f * character.scale
+                )
                 shapeDrawer.line(
                     leftShoulder + leftUpperArmVector,
                     leftHandGripPoint,
@@ -335,3 +416,31 @@ sealed class RenderableType {
     }
 
 }
+
+/**
+ * What is a leftarm?
+ *
+ * It is a bunch of points that are defined somehow
+ * that we then want to draw lines and circles in relation to
+ *
+ * So, the left arm consists of:
+ *
+ * The Right Shoulder: a vector
+ * The Left Shoulder: a vector
+ * An Aim Vector
+ * A Grip Point - defined as a distance N from the right shoulder in the direction of the aim vector
+ * An upper arm - defined as a unit vector pointing from shoulder to grip point, rotated by angle Beta and scaled by the length of the upper arm
+ *
+ * OK; this actually seems quite hard to generalize, because it isn't general
+ *
+ * This is the definition of the LEFT arm, but the RIGHT arm doesn't need the left shoulder
+ * and so on. Having them in their own functions is probably fine for now, but there is probably
+ * some sweet generalizations to be made here.
+ *
+ * Lets do a torso!
+ */
+
+class ParameterizedVectorThingie {
+
+}
+
